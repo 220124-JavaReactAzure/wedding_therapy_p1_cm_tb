0c6e5ac7851a4b334bc119bdcbaff2bdc336c076
tbodine <39622946+tbodine@users.noreply.github.com>
2022-02-17 15:20:07 -0600

    added Weddings model and removed the weddings name field from betrothed model

--- weddingTherapy/src/main/java/com/revature/wedding_therapy/models/Bethrothed.java
+++ weddingTherapy/src/main/java/com/revature/wedding_therapy/models/Bethrothed.java
@@ -15,9 +15,6 @@ public class Bethrothed {
 	@Column(name = "wedding_id")
 	private int wedding_id;
 
-	@Column(name = "wedding_name")
-	private String wedding_name;
-
 	@Column(name = "wedding_roll")
 	private String wedding_roll;
 
@@ -28,17 +25,16 @@ public class Bethrothed {
 	private String last_name;
 
 	// CONSTRUCTORS
-	// No-Arg Constructor
+	// Default Constructor
 	public Bethrothed() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
 	// Full-Arg Constructor
-	public Bethrothed(int wedding_id, String wedding_name, String wedding_roll, String first_name, String last_name) {
+	public Bethrothed(int wedding_id, String wedding_roll, String first_name, String last_name) {
 		super();
 		this.wedding_id = wedding_id;
-		this.wedding_name = wedding_name;
 		this.wedding_roll = wedding_roll;
 		this.first_name = first_name;
 		this.last_name = last_name;
@@ -51,14 +47,6 @@ public class Bethrothed {
 
 	public void setWedding_id(int wedding_id) {
 		this.wedding_id = wedding_id;
-	}
-
-	public String getWedding_name() {
-		return wedding_name;
-	}
-
-	public void setWedding_name(String wedding_name) {
-		this.wedding_name = wedding_name;
 	}
 
 	public String getWedding_roll() {
@@ -88,14 +76,14 @@ public class Bethrothed {
 	// TOSTRING METHOD
 	@Override
 	public String toString() {
-		return "Bethrothed [wedding_id=" + wedding_id + ", wedding_name=" + wedding_name + ", wedding_roll="
+		return "Bethrothed [wedding_id=" + wedding_id + ", wedding_roll="
 				+ wedding_roll + ", first_name=" + first_name + ", last_name=" + last_name + "]";
 	}
 		
 	//HASHCODE
 	@Override
 	public int hashCode() {
-		return Objects.hash(first_name, last_name, wedding_id, wedding_name, wedding_roll);
+		return Objects.hash(first_name, last_name, wedding_id, wedding_roll);
 	}
 
 	@Override
@@ -108,8 +96,7 @@ public class Bethrothed {
 			return false;
 		Bethrothed other = (Bethrothed) obj;
 		return Objects.equals(first_name, other.first_name) && Objects.equals(last_name, other.last_name)
-				&& wedding_id == other.wedding_id && Objects.equals(wedding_name, other.wedding_name)
-				&& Objects.equals(wedding_roll, other.wedding_roll);
+				&& wedding_id == other.wedding_id && Objects.equals(wedding_roll, other.wedding_roll);
 	}
 	
 }
