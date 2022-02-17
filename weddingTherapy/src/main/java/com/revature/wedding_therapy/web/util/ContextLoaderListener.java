1dfae8a44df627052393334f3227d931d27ef16d
ChristianMains <crmains@protonmail.com>
2022-02-16 19:54:25 -0600

    Got Test Servlet working

---
+++ weddingTherapy/src/main/java/com/revature/wedding_therapy/web/util/ContextLoaderListener.java
@@ -0,0 +1,29 @@
+package com.revature.wedding_therapy.web.util;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebListener
+public class ContextLoaderListener implements ServletContextListener{
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		
+		
+		ServletContext context = sce.getServletContext();
+		//context.addServlet("ServletName", NameServlet).addMapping("/newpath/*");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		ServletContextListener.super.contextDestroyed(sce);
+	}
+}
