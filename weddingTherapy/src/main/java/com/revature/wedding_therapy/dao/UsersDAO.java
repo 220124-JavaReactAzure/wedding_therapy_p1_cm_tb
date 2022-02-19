7afed0689d3af9f751794fbf062617820cf2cb15
tbodine <39622946+tbodine@users.noreply.github.com>
2022-02-19 17:39:44 -0600

    Updated Users model, UsersDAO, UsersService, UsersServlet and contesx listner files.

--- weddingTherapy/src/main/java/com/revature/wedding_therapy/dao/UsersDAO.java
+++ weddingTherapy/src/main/java/com/revature/wedding_therapy/dao/UsersDAO.java
@@ -1,5 +1,48 @@
 package com.revature.wedding_therapy.dao;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.wedding_therapy.models.Users;
+import com.revature.wedding_therapy.util.HibernateUtil;
+
 public class UsersDAO {
+	
+	public boolean createNewUser(Users newUsers) {
+		try {
+			
+			Session session = HibernateUtil.getSession();
+			System.out.println("\n\n\nUsersDAO:createNewUsers\n\n\n"+newUsers.toString());
+			Transaction trans = session.beginTransaction();
+			session.save(newUsers);
+			trans.commit();            // Make sure to put transactions around all DAO actions so it works.
+			
+			return true;
+		} catch (HibernateException | IOException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
+	
+	
+	public List<Users> getAllUsers(){
+		try {
+			System.out.print("\n\n\n\n\nUsersDAO:getAllUsers\n\n\n\n\n");
+			Session session = HibernateUtil.getSession();
+			List<Users> answer = session.createQuery("FROM users").list();
+			return answer;
+		} catch (HibernateException | IOException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
 
 }
--- weddingTherapy/src/main/java/com/revature/wedding_therapy/web/util/ContextLoaderListener.java
+++ weddingTherapy/src/main/java/com/revature/wedding_therapy/web/util/ContextLoaderListener.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.revature.wedding_therapy.dao.*;
 import com.revature.wedding_therapy.services.*;
 import com.revature.wedding_therapy.web.servlet.*;
@@ -17,13 +18,19 @@ public class ContextLoaderListener implements ServletContextListener{
 	public void contextInitialized(ServletContextEvent sce) {
 		
 		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new Hibernate5Module());
 		
 		EmployeeDAO employeeDAO = new EmployeeDAO();
 		EmployeeService employeeService = new EmployeeService(employeeDAO);
 		EmployeeServlet employeeServlet = new EmployeeServlet(mapper, employeeService);
 		
+		UsersDAO usersDAO = new UsersDAO();
+		UsersService usersService = new UsersService(usersDAO);
+		UsersServlet usersServlet = new UsersServlet(mapper, usersService);
+		
 		ServletContext context = sce.getServletContext();
 		context.addServlet("EmployeeServlet", employeeServlet).addMapping("/employee/*");
+		context.addServlet("UserServlet", usersServlet).addMapping("/users/*");
 	}
 	
 	@Override
